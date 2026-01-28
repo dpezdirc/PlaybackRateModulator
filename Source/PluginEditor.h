42bf264c8ada4601654aed7589747ad64eda3181
dpezdirc <105217062+dpezdirc@users.noreply.github.com>
2026-01-28 17:26:37 +0100

    Formatting/cleanup

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,33 +1,24 @@
-/*
-  ==============================================================================
-
-    This file contains the basic framework code for a JUCE plugin editor.
-
-  ==============================================================================
-*/
+//------------------------------------------------------------------------------
+//  Code for the plugin's GUI
+//------------------------------------------------------------------------------
 
 #pragma once
 
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
-//==============================================================================
-/**
-*/
-class PlaybackRateModulatorAudioProcessorEditor  : public juce::AudioProcessorEditor
+//------------------------------------------------------------------------------
+class PlaybackRateModulatorAudioProcessorEditor : public juce::AudioProcessorEditor
 {
 public:
-    PlaybackRateModulatorAudioProcessorEditor (PlaybackRateModulatorAudioProcessor&);
-    ~PlaybackRateModulatorAudioProcessorEditor() override;
+	PlaybackRateModulatorAudioProcessorEditor(PlaybackRateModulatorAudioProcessor&);
+	~PlaybackRateModulatorAudioProcessorEditor() override {}
 
-    //==============================================================================
-    void paint (juce::Graphics&) override;
-    void resized() override;
+	void paint(juce::Graphics&) override;
+	void resized() override;
 
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
-    PlaybackRateModulatorAudioProcessor& audioProcessor;
+	PlaybackRateModulatorAudioProcessor& audioProcessor;
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PlaybackRateModulatorAudioProcessorEditor)
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlaybackRateModulatorAudioProcessorEditor)
 };
